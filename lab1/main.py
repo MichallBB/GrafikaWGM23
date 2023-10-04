8cee054c6d6b6901412c0453edbfd8747935ab1c
MichallBB <98035204+MichallBB@users.noreply.github.com>
2023-10-04 09:21:39 +0200

    Add files via upload

---
+++ lab1/main.py
@@ -0,0 +1,45 @@
+import numpy as np
+from PIL import Image
+
+
+obrazek = Image.open("mb.bmp")
+print("tryb: ", obrazek.mode)
+print("rozmiar: ", obrazek.size)
+print("format: ", obrazek.format)
+
+dane_obrazka = np.asarray(obrazek)
+dane_obrazka1 = dane_obrazka * 1
+
+# zad 3
+# zapis tablicy do pliku
+t1_text = open('inicjaly.txt', 'w')
+for rows in dane_obrazka1:
+    for item in rows:
+        t1_text.write(str(item) + ' ')
+    t1_text.write('\n')
+t1_text.close()
+
+# Zad 4
+print("---------------- informacje o tablicy obrazu----------------")
+print("typ danych tablicy:", dane_obrazka.dtype)
+print("rozmiar tablicy:", dane_obrazka.shape)
+print("liczba elementow:", dane_obrazka.size)
+print("wymiar tablicy:", dane_obrazka.ndim)
+print("rozmiar wyrazu tablicy:",dane_obrazka.itemsize)
+print("wyraz (49,30):", dane_obrazka[30][49])
+print("wyraz (90,40):", dane_obrazka[40][90])
+print("wyraz (99,0):", dane_obrazka[0][99])
+print("************************\n")
+# Zad 5
+t1 = np.loadtxt("inicjaly.txt", dtype=np.bool_)
+print("typ danych tablicy t1:", t1.dtype)
+print("rozmiar tablicy t1 :", t1.shape)
+print("wymiar tablicy t1 :", t1.ndim)
+
+# Zad 6
+t3 = np.loadtxt("inicjaly.txt", dtype=np.uint8)
+print("\ntyp danych tablicy t3:", t3.dtype)
+print("rozmiar tablicy t3 :", t3.shape)
+print("wymiar tablicy t3 :", t3.ndim)
+obraz = Image.fromarray(t3)
+obraz.show()
