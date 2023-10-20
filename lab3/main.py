9f1eaa108b28d9fb63179b5c7d3bf03c1230805d
MichallBB <98035204+MichallBB@users.noreply.github.com>
2023-10-20 14:32:55 +0200

    Add files via upload

--- lab3/main.py
+++ lab3/main.py
@@ -44,3 +44,55 @@ pionowe.save("obraz1_2.png")
 pionowe_neg = Image.fromarray(negatyw_szare(pionowe))
 pionowe_neg.save("obraz1_2N.jpg")
 pionowe_neg.save("obraz1_2N.png")
+
+def pionowe_paski_RGB(w, h, grub):
+    t = (h, w, 3)
+    tab = np.zeros(t, dtype=np.uint8)
+    ile2 = int(w / grub)
+    j = 0
+    for k in range(ile2+1):
+        if k%2 == 0:
+            tab[:,j:j+grub] = [0,255,random.randint(0,255)]
+        else:
+            tab[:, j:j + grub] = [255, random.randint(0,255), 0]
+        j = k * grub
+    return Image.fromarray(tab)
+
+pionowe_RGB = pionowe_paski_RGB(480, 320, 15)
+test = np.asarray(pionowe_RGB)
+pionowe_RGB.show()
+
+def negatyw_RGB(obraz):
+    tab = np.asarray(obraz)
+    h, w, c = tab.shape
+    tab_neg = tab.copy()
+    for i in range(h):
+        for j in range(w):
+            for k in range(c):
+                tab_neg[i, j, k] = 255 - tab[i, j, k]
+    return Image.fromarray(tab_neg)
+
+pionowe_RGB_negatyw = negatyw_RGB(pionowe_RGB)
+pionowe_RGB_negatyw.show()
+
+inicjaly = Image.open("inicjaly.bmp")
+
+def koloruj_obraz(obraz, grub):
+    t_obraz = np.asarray(obraz)
+    h, w = t_obraz.shape
+    t =(h, w, 3)
+    tab = np.ones(t, dtype=np.uint8)
+    for i in range(h):
+        for j in range(w):
+            if t_obraz[i, j] == False:
+                if i % 2 == 0:
+                    tab[i, j] = [66,5,23]
+                else:
+                    tab[i, j] = [66, 111, 88]
+            else:
+                tab[i, j] = [255, 255, 255]
+    return tab
+
+#gwiazdka = Image.open("gwiazdka.bmp")
+obraz2 = Image.fromarray(koloruj_obraz(inicjaly, 10))
+obraz2.show()
