9f4e8ee4faf0ccb33e5659be446e0e6e7817b388
MichallBB <98035204+MichallBB@users.noreply.github.com>
2023-12-11 21:15:16 +0100

    k

--- lab9/main.py
+++ lab9/main.py
@@ -36,22 +36,15 @@ def histogram_equalization(obraz):
     equalized_image = equalized_image.astype(np.uint8)
     return Image.fromarray(equalized_image)
 
+
     # 2.4
 if __name__ == "__main__":
     im = Image.open("zeby.png")
-
     obraz_l = im.convert("L")
-
     normalized_hist = histogram_norm(obraz_l)
-    print("Histogram norm:")
-    print(normalized_hist)
-
     cumulated_hist = histogram_cumul(obraz_l)
-    print("\nHistogram cumul:")
-    print(cumulated_hist)
 
     equalized_image = histogram_equalization(obraz_l)
-    equalized_image.show()
     equalized_image.save("equalized.png")
 
     plt.figure(figsize=(12, 6))
@@ -69,72 +62,92 @@ if __name__ == "__main__":
     plt.plot(cumulated_hist)
 
     plt.subplot(2, 3, 4)
-    plt.title("Eq Image")
+    plt.title("Equalized Image")
     plt.imshow(equalized_image, cmap='gray')
 
     equalized_hist = histogram_norm(equalized_image)
     plt.subplot(2, 3, 5)
-    plt.title("Eq Histogram")
+    plt.title("Equalized Histogram")
     plt.plot(equalized_hist)
 
     plt.subplot(2, 3, 6)
-    plt.title("Eq Cumul Histogram")
+    plt.title("Equalized Cumul Histogram")
     equalized_cumul_hist = histogram_cumul(equalized_image)
     plt.plot(equalized_cumul_hist)
 
     plt.savefig("fig1.png")
-    plt.show()
 
+
+    print("Obraz L:")
     statystyki(obraz_l)
+    print("\nEqualized image:")
     statystyki(equalized_image)
 
     # 3
-    equalized_image_op = ImageOps.equalize(obraz_l)
-    equalized_image_op.save("equalized1.png")
+    equalized_image_ops = ImageOps.equalize(obraz_l)
+    equalized_image_ops.save("equalized1.png")
 
     plt.figure(figsize=(12, 6))
 
     plt.subplot(1, 3, 1)
-    plt.title("Original Image")
+    plt.title("Orginalny obraz L")
     plt.imshow(obraz_l, cmap='gray')
 
     plt.subplot(1, 3, 2)
-    plt.title("Equalized Image (Custom)")
+    plt.title("Equalized Image")
     plt.imshow(equalized_image, cmap='gray')
 
     plt.subplot(1, 3, 3)
     plt.title("Equalized Image (ImageOps)")
-    plt.imshow(equalized_image_op, cmap='gray')
+    plt.imshow(equalized_image_ops, cmap='gray')
 
     plt.savefig("fig2.png")
-    plt.show()
+    # plt.show()
 
 
     #  4
     def konwertuj1(obraz, w_r, w_g, w_b):
-        img_array = np.array(obraz)
-        l_channel = np.round(img_array[:, :, 0] * w_r + img_array[:, :, 1] * w_g + img_array[:, :, 2] * w_b).astype(
-            np.uint8)
-        return Image.fromarray(l_channel)
+        if abs(w_r + w_g + w_b - 1) > 0.001:  # Porównanie z małą wartością bezwzględną
+            raise ValueError("Suma wag w_r, w_g, w_b musi być równa 1")
 
+        img = obraz.convert('RGB')
+        img_l = Image.new('L', img.size)
 
-    mgla_image = Image.open("mgla.jpg")
-    converted_image1 = konwertuj1(mgla_image, 0.299, 0.587, 0.114)
-    converted_image1.save("mgla_L1.png")
+        for x in range(img.width):
+            for y in range(img.height):
+                r, g, b = img.getpixel((x, y))
+                l = int(round(r * w_r + g * w_g + b * w_b))
+                img_l.putpixel((x, y), l)
 
-    converted_image = mgla_image.convert('L')
-    converted_image.save("mgla_L.png")
-
-    statystyki(converted_image)
-    statystyki(converted_image1)
+        return img_l
 
     def konwertuj2(obraz, w_r, w_g, w_b):
-        img_array = np.array(obraz)
-        l_channel = (img_array[:, :, 0] * w_r + img_array[:, :, 1] * w_g + img_array[:, :, 2] * w_b).astype(int)
-        return Image.fromarray(l_channel)
+        if abs(w_r + w_g + w_b - 1) > 0.001:
+            raise ValueError("Suma wag w_r, w_g, w_b musi być równa 1")
+        img = obraz.convert('RGB')
+        img_l = Image.new('L', img.size)
 
+        for x in range(img.width):
+            for y in range(img.height):
+                r, g, b = img.getpixel((x, y))
+                l = int(r * w_r + g * w_g + b * w_b)
+                img_l.putpixel((x, y), l)
+        return img_l
 
-    converted_image2 = konwertuj2(mgla_image, 0.299, 0.587, 0.114)
-    converted_image2.save("mgla_L2.png")
+    obraz = Image.open('mgla.jpg')
 
-    statystyki(converted_image2)
+    mgla_L1 = konwertuj1(obraz, 0.299, 0.587, 0.114)
+    mgla_L1.save('mgla_L1.png')
+
+    mgla_L = obraz.convert('L')
+    mgla_L.save('mgla_L.png')
+
+    mgla_L2 = konwertuj2(obraz, 0.299, 0.587, 0.114)
+    mgla_L2.save('mgla_L2.png')
+
+    print("\nMgla L:")
+    statystyki(mgla_L)
+    print("\nMgla L1:")
+    statystyki(mgla_L1)
+    print("\nMgla L2")
+    statystyki(mgla_L2)
